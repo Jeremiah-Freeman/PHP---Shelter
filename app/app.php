bfe783c0524e24d9b9e838aaca643d46e06707e5
Jay Freeman and Zach Swanson <zach@>
2017-02-21 15:14:24 -0800

    setup app.php routing, twig display and functioning input and submit code

--- app/app.php
+++ app/app.php
@@ -1,7 +1,10 @@
 <?php
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
-    
+    require_once __DIR__."/../src/Type.php";
+    require_once __DIR__."/../src/Animal.php";
+
+
 
     $app = new Silex\Application();
     $app['debug']=true;
@@ -14,6 +17,17 @@
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render("index.html.twig",array('types'=>Type::getAll(),'animals'=>Animal::getAll()));
+    });
+
+    $app->post("/", function() use ($app) {
+        $new_type = new Type($_POST['type']);
+        $new_type->save();
+        return $app['twig']->render("index.html.twig",array('types'=>Type::getAll(),'animals'=>Animal::getAll()));
+    });
+
 
 
     return $app;

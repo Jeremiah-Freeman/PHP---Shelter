7b360f3b010af1e3fde22ba2d06d65a31c945677
Jay Freeman and Zach Swanson <jay@>
2017-02-21 14:08:16 -0800

    add tests and methods for Animal class and getter methods

--- src/Animal.php
+++ src/Animal.php
@@ -1,6 +1,52 @@
 <?php
-    
+
+class Animal
+    {
+        private $name;
+        private $age;
+        private $date_of_admittance;
+        private $id_type;
+        private $id;
+
+        function __construct($name,$age,$date_of_admittance,$id_type,$id=null)
+        {
+            $this->name = $name;
+            $this->age = $age;
+            $this->date_of_admittance = $date_of_admittance;
+            $this->id_type = $id_type;
+            $this->id = $id;
+        }
+        function getName()
+        {
+            return $this->name;
+        }
+        function getAge()
+        {
+            return $this->age;
+        }
+        function getDateOfAdmittance()
+        {
+            return $this->date_of_admittance;
+        }
+        function getIdType()
+        {
+            return $this->id_type;
+        }
+        function getId()
+        {
+            return $this->id;
+        }
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO animals(name,age,date_of_admittance,id_type)VALUES('{$this->getName()}',{$this->getAge()},'{$this->getDateOfAdmittance()}',{$this->getIdType()})");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM animals;");
+        }
 
 
+}
 
 ?>
--- tests/AnimalTest.php
+++ tests/AnimalTest.php
@@ -1,28 +1,93 @@
 <?php
 
     /**
-    * @backGlobals disabled
-    * @backupstaticAttributes disabled
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
     */
 
-    // // require_once "src/Animal.php";
-    // // require_once "src/Type.php";
-    //
-    // $server= 'mysql:host=localhost:8889;dbname=shelter_test';
-    // $username= 'root';
-    // $password = 'root';
-    // $DB = new PDO($server, $username, $password);
-    //
-    // class AnimalTest extends PHPUnit_Framework_TestCase
-    // {
-    //     protected function tearDown()
-    //     {
-    //         // Animal::deleteAll();
-    //         // Type::deleteAll();
-    //     }
-    //
-    // }
-    //
-    //
+    require_once "src/Animal.php";
+    require_once "src/Type.php";
+
+    $server= 'mysql:host=localhost:8889;dbname=shelter_test';
+    $username= 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class AnimalTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            Animal::deleteAll();
+            Type::deleteAll();
+        }
+
+        function test_animal_getters()
+        {
+            //Arrange
+            $type = "lion";
+            $id = 1;
+            $test_type = new Type($type,$id);
+            $name = "Mr. Lion";
+            $age = 50;
+            $date_of_admittance = '2017-02-21';
+            $id_type = $test_type->getId();
+            $test_animal = new Animal($name,$age,$date_of_admittance,$id_type,$id);
+
+
+            //Act
+            $test_values = array(
+                'name' => "Mr. Lion",
+                'age'=>50,
+                'date_of_admittance'=>'2017-02-21',
+                'id_type'=>1,
+                'id' => 1
+            );
+            $result = array(
+                'name' => $test_animal->getName(),
+                'age'=>$test_animal->getAge(),
+                'date_of_admittance'=>$test_animal->getDateOfAdmittance(),
+                'id_type'=>$test_animal->getIdType(),
+                'id' => $test_animal->getId()
+            );
+
+            //Assert
+            $this->assertEquals($test_values,$result);
+        }
+
+        // function test_save()
+        // {
+        //     //Arrange
+        //     $type = "lion";
+        //     $test_type = new Type($type);
+        //     $test_type-> save();
+        //
+        //     //Act
+        //     $result = Type::getAll();
+        //
+        //     //Assert
+        //     $this->assertEquals($test_type,$result[0]);
+        // }
+        // function test_getAll()
+        // {
+        //     //Arrange
+        //     $type = "lion";
+        //     $type2 = "shark";
+        //     $test_type = new Type($type);
+        //     $test_type->save();
+        //     $test_type2 = new Type($type2);
+        //     $test_type2->save();
+        //
+        //     //Act
+        //     $result = Type::getAll();
+        //
+        //     //Assert
+        //     $this->assertEquals([$test_type,$test_type2], $result);
+        //
+        // }
+
+
+    }
+
+
 
 ?>
